e320846fe3b5fe82f3c2ecfad1d3cf89ab06e26e
Christian <christian.rojas@pti-sa.com.co>
2025-01-29 15:48:53 -0500

    Se crea rama de conexión a base de datos

---
+++ src/test/java/com/pti_sa/inventory_system/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pti_sa.inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
