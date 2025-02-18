6539466c90af07de244e13644014a9609021accc
Christian <christian.rojas@pti-sa.com.co>
2025-02-18 14:33:25 -0500

    Instalación y entorno desarrollo react

--- src/main/java/com/pti_sa/inventory_system/application/dto/request/UserRequestDTO.java
+++ src/main/java/com/pti_sa/inventory_system/application/dto/request/UserRequestDTO.java
@@ -1,9 +1,5 @@
 package com.pti_sa.inventory_system.application.dto.request;
 
-import com.pti_sa.inventory_system.domain.model.UserType;
-
-import java.util.List;
-
 public record UserRequestDTO(
         String email,
         String password
